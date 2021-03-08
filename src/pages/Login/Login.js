b42dc4fad3eb0f1c28469a178debbca3f80ea7e3
Augusto Luna <caugustolunas@gmail.com>
2021-03-08 11:57:00 -0300

    Adicionei os controles dos inputs da pagina de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,28 +1,52 @@
 import React from "react";
 import { useHistory } from "react-router";
+import useForm from "../../hooks/useForm";
 import {
   MainDiv,
   LoginDiv,
   Title,
-  NameNickname,
+  Email,
   Password,
   LoginButton,
   GoBackButton,
+  LoginForm,
 } from "../Login/Styled";
 
 const Login = () => {
+  const [form, onChange, clear] = useForm({ email: "", password: "" });
+
   const history = useHistory();
 
   const goBack = () => {
     history.goBack();
   };
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+  };
   return (
     <MainDiv>
       <LoginDiv>
         <Title> FAÇA SEU LOGIN </Title>
-        <NameNickname placeholder="Nome ou Nickname" type="text" />
-        <Password placeholder="Senha" type="password" />
-        <LoginButton> LOGIN </LoginButton>
+        <LoginForm onSubmit={onSubmitForm}>
+          <Email
+            placeholder="Email"
+            type="email"
+            name={"email"}
+            value={form.email}
+            onChange={onChange}
+            required
+          />
+          <Password
+            placeholder="Senha"
+            type="password"
+            name={"password"}
+            value={form.password}
+            onChange={onChange}
+            required
+          />
+          <LoginButton type={"submit"}> LOGIN </LoginButton>
+        </LoginForm>
         <GoBackButton onClick={goBack}> VOLTAR </GoBackButton>
       </LoginDiv>
     </MainDiv>
