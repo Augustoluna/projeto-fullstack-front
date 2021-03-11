df82b89ca8e494afd549f62791201133eb037b02
Augusto Luna <caugustolunas@gmail.com>
2021-03-11 19:07:09 -0300

    Comecei a montar a requisicao da pagina AllImages

--- src/pages/AllImages/AllImages.js
+++ src/pages/AllImages/AllImages.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { BASE_URL } from "../../constants/urls";
+import ImageCard from "../../components/ImageCard/ImageCard";
 import useProtectedPage from "../../hooks/useProtectedPage";
+import useRequestData from "../../hooks/useRequestData";
+import { MainDiv } from "./Styled";
 
 const AllImages = () => {
   useProtectedPage();
-  return (
-    <div>
-      <p>Página de todas as imagens</p>
-    </div>
-  );
+  const images = useRequestData([], `${BASE_URL}/image/all`);
+  console.log(images);
+
+  const imageCards = images.map((image) => {
+    return (
+      <ImageCard
+        key={image.id}
+        title={image.subtitle}
+        image={image.file}
+        onClick={() => null}
+      />
+    );
+  });
+
+  return <MainDiv>{imageCards}</MainDiv>;
 };
 
 export default AllImages;
---
+++ src/pages/AllImages/Styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
